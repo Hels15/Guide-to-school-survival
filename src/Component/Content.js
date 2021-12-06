a106c52f6db2a2468a132f4f8e7ea7807309ea36
Hadházy Tamás <helsgithub15@gmail.com>
2021-12-06 20:12:04 +0100

    fix button

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -6,12 +6,10 @@ import SaveIcon from "@mui/icons-material/Save";
 function Content() {
     return (
         <div className="content-container">
-
                          <div className="left-side-content">
                              <motion.div className="school-stress" initial={{x: "-100vw"}} animate={{x:0}} transition={{delay: 1.5, duration:1.5}}>HELP WITH SCHOOL STRESS</motion.div>
                               <motion.div className="look-around" initial={{x: "-100vw"}} animate={{x:0}} transition={{delay: 1.5}}><h1>Look around and <br/> use help</h1></motion.div>
                         </div>
-
                         <div className="right-side-content">
                             <div className="site-milestones">
                                 SITE MILESTONES
@@ -20,7 +18,6 @@ function Content() {
                                 More Information
                             </Button>
                         </div>
-
         </div>
     );
 }
--- src/Component/Substance.js
+++ src/Component/Substance.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./style/main.scss"
-
+import Button from "@mui/material/Button";
 
 
 function Substance(props) {
     return (
         <div className="base-content-container content-container">Substance
-            <button>test</button>
+            <Button variant="contained" color="primary">test</Button>
         </div>
 
     );
