d56172d8150e8707736d7180a0b3c6dc65203cfe
Hadházy Tamás <helsgithub15@gmail.com>
2021-12-06 20:45:26 +0100

    creating a microcomponent and rudimentary Tab with a material ui.

--- src/Component/Substance.js
+++ src/Component/Substance.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import "./style/main.scss"
 import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import { TabContext } from '@mui/lab';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import {useState} from "react";
+
+function Table1(props) {
+    return (
+        <div></div>
+    );
+}
+
+function Table2(props) {
+    return (
+        <div></div>
+    );
+}
+function Table3(props) {
+    return (
+        <div></div>
+    );
+}
 
 
 function Substance(props) {
+
+    const [value, setValue] = useState('1')
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    };
+
     return (
-        <div className="base-content-container content-container">Substance
+        <Box sx={{ width: '100%',typography:'body1'}}>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab label="Item One" value="1" />
+                        <Tab label="Item Two" value="2" />
+                        <Tab label="Item Three" value="3" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">Item One</TabPanel>
+                <TabPanel value="2">Item Two</TabPanel>
+                <TabPanel value="3">Item Three</TabPanel>
+            </TabContext>
+            <div className="base-content-container content-container">Substance
             <Button variant="contained" color="primary">test</Button>
         </div>
+        </Box>
+
 
     );
 }
